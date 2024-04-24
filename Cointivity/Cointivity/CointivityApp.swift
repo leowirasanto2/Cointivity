9e4a34b58ff328864d703ccc9d868b0d45db50f8
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-04-25 00:01:32 +0800

    initial commit

---
+++ Cointivity/Cointivity/CointivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CointivityApp.swift
+//  Cointivity
+//
+//  Created by Leo Wirasanto Laia on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CointivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
